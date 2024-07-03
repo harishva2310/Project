218fe26326faa0d631fc547b863667203d3bed06
Harish Vodeyavargam Anand <harish.va1910@gmail.com>
2024-07-03 15:59:56 -0400

    Run shell scripts

--- frontend/vehicle-rental/run.sh
+++ frontend/vehicle-rental/run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-sudo docker rmi --force harishva1910/frontend|| true
-
-sudo docker build --no-cache -t harishva1910/frontend:latest .
-
-sudo docker push harishva1910/frontend:latest
-
-sudo docker run -p 80:80 harishva1910/frontend:latest
+HTTPS=true npm start
