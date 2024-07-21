41896390357d1c2ef4d9d57e47380bc07d9791fc
Harish Vodeyavargam Anand <harish.va1910@gmail.com>
2024-07-21 13:24:39 -0400

    updated redis config

--- dev/src/main/java/com/homerental/dev/config/RedisConfig.java
+++ dev/src/main/java/com/homerental/dev/config/RedisConfig.java
@@ -2,20 +2,23 @@ package com.homerental.dev.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("150.136.151.38", 6379);
+        config.setPassword("ABC_123"); // If you have set a password
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
 }
