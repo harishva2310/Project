e9d833989c7e59e45351ccc44401dd6e10f09170
Harish Vodeyavargam Anand <harish.va1910@gmail.com>
2024-09-19 16:50:58 -0400

    Update CORS config

--- dev/src/main/java/com/homerental/dev/config/WebConfig.java
+++ dev/src/main/java/com/homerental/dev/config/WebConfig.java
@@ -10,8 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins(System.getenv("ALLOWED_ORIGIN"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
                 
     }
 }
